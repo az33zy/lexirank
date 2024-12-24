5c767b2194f35ecd7bd4bf6ef4e6732f728135d2
az33zy <10743275+az33zy@users.noreply.github.com>
2024-12-24 20:35:43 +0500

    format files

--- src/index.js
+++ src/index.js
@@ -11,137 +11,137 @@
  * @implements {CharacterSet}
  */
 export class AsciiPrintableCharSet {
-  min() {
-    return '!'; // 33
-  }
-  mid() {
-    return 'O'; // 79
-  }
-  max() {
-    return '~'; // 126
-  }
+	min() {
+		return "!" // 33
+	}
+	mid() {
+		return "O" // 79
+	}
+	max() {
+		return "~" // 126
+	}
 
-  /**
-   * @param {string} prevChar
-   * @param {string} nextChar
-   */
-  between(prevChar, nextChar) {
-    if (prevChar === nextChar) return null;
+	/**
+	 * @param {string} prevChar
+	 * @param {string} nextChar
+	 */
+	between(prevChar, nextChar) {
+		if (prevChar === nextChar) return null
 
-    const prevCharCode = prevChar.charCodeAt(0);
-    const nextCharCode = nextChar.charCodeAt(0);
+		const prevCharCode = prevChar.charCodeAt(0)
+		const nextCharCode = nextChar.charCodeAt(0)
 
-    if (prevCharCode === nextCharCode - 1) return null;
+		if (prevCharCode === nextCharCode - 1) return null
 
-    const midCharCode =
-      prevCharCode + Math.floor((nextCharCode - prevCharCode) / 2);
+		const midCharCode =
+			prevCharCode + Math.floor((nextCharCode - prevCharCode) / 2)
 
-    const midChar = String.fromCharCode(midCharCode);
+		const midChar = String.fromCharCode(midCharCode)
 
-    return midChar;
-  }
+		return midChar
+	}
 
-  /**
-   * @param {string} str
-   */
-  isValid(str) {
-    for (let i = 0; i < str.length; i++) {
-      if (str[i] < this.min() || str[i] > this.max()) {
-        return false;
-      }
-    }
-    return true;
-  }
+	/**
+	 * @param {string} str
+	 */
+	isValid(str) {
+		for (let i = 0; i < str.length; i++) {
+			if (str[i] < this.min() || str[i] > this.max()) {
+				return false
+			}
+		}
+		return true
+	}
 }
 
 export class LexiRank {
-  charSet;
+	charSet
 
-  /**
-   * @param {CharacterSet} charSet
-   */
-  constructor(charSet) {
-    this.charSet = charSet;
-  }
+	/**
+	 * @param {CharacterSet} charSet
+	 */
+	constructor(charSet) {
+		this.charSet = charSet
+	}
 
-  between(prev = this.charSet.min(), next = this.charSet.max()) {
-    this.assert(prev, next);
+	between(prev = this.charSet.min(), next = this.charSet.max()) {
+		this.assert(prev, next)
 
-    let rank = '';
-    let i = 0;
+		let rank = ""
+		let i = 0
 
-    while (true) {
-      const prevChar = prev[i] ?? this.charSet.min();
-      const nextChar = next[i] ?? this.charSet.max();
+		while (true) {
+			const prevChar = prev[i] ?? this.charSet.min()
+			const nextChar = next[i] ?? this.charSet.max()
 
-      const midChar = this.charSet.between(prevChar, nextChar);
+			const midChar = this.charSet.between(prevChar, nextChar)
 
-      if (midChar === null) {
-        rank += prevChar;
-        i++;
-        continue;
-      }
+			if (midChar === null) {
+				rank += prevChar
+				i++
+				continue
+			}
 
-      rank += midChar;
-      break;
-    }
+			rank += midChar
+			break
+		}
 
-    return rank;
-  }
+		return rank
+	}
 
-  /**
-   * Alias for `.between(undefined, 'next')`
-   * @param {string} next
-   */
-  before(next) {
-    return this.between(undefined, next);
-  }
+	/**
+	 * Alias for `.between(undefined, 'next')`
+	 * @param {string} next
+	 */
+	before(next) {
+		return this.between(undefined, next)
+	}
 
-  /**
-   * Alias for `.between('prev', undefined)`
-   * @param {string} prev
-   */
-  after(prev) {
-    return this.between(prev, undefined);
-  }
+	/**
+	 * Alias for `.between('prev', undefined)`
+	 * @param {string} prev
+	 */
+	after(prev) {
+		return this.between(prev, undefined)
+	}
 
-  mid() {
-    return this.between();
-  }
+	mid() {
+		return this.between()
+	}
 
-  /**
-   * @param {string} prev
-   * @param {string} next
-   */
-  assert(prev, next) {
-    if (typeof prev !== 'string' || typeof next !== 'string') {
-      throw new LexiRankInputError(`prev and next can only be string`);
-    }
+	/**
+	 * @param {string} prev
+	 * @param {string} next
+	 */
+	assert(prev, next) {
+		if (typeof prev !== "string" || typeof next !== "string") {
+			throw new LexiRankInputError(`prev and next can only be string`)
+		}
 
-    if (prev === '' || next === '') {
-      throw new LexiRankInputError(`prev and next can't be an empty string`);
-    }
+		if (prev === "" || next === "") {
+			throw new LexiRankInputError(`prev and next can't be an empty string`)
+		}
 
-    if (prev >= next) {
-      throw new LexiRankInputError(`prev should be smaller than next`);
-    }
+		if (prev >= next) {
+			throw new LexiRankInputError(`prev should be smaller than next`)
+		}
 
-    if (!this.charSet.isValid(prev)) {
-      throw new LexiRankInputError(`prev contains invalid characters`);
-    }
+		if (!this.charSet.isValid(prev)) {
+			throw new LexiRankInputError(`prev contains invalid characters`)
+		}
 
-    if (!this.charSet.isValid(next)) {
-      throw new LexiRankInputError(`next contains invalid characters`);
-    }
-  }
+		if (!this.charSet.isValid(next)) {
+			throw new LexiRankInputError(`next contains invalid characters`)
+		}
+	}
 }
 
 export class LexiRankInputError extends Error {
-  /**
-   * @param {string} message
-   */
-  constructor(message) {
-    super(message);
-    this.name = 'LexiRankInputError';
-  }
+	/**
+	 * @param {string} message
+	 */
+	constructor(message) {
+		super(message)
+		this.name = "LexiRankInputError"
+	}
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,85 +1,85 @@
-import { test, expect } from 'vitest';
-import { LexiRank, AsciiPrintableCharSet } from '../src';
+import { test, expect } from "vitest"
+import { LexiRank, AsciiPrintableCharSet } from "../src"
 
-const lexirank = new LexiRank(new AsciiPrintableCharSet());
+const lexirank = new LexiRank(new AsciiPrintableCharSet())
 
-test('between()', () => {
-  expect(lexirank.between('A', 'C')).toBe('B');
-  expect(lexirank.between('#', '3')).toBe('+');
-  expect(lexirank.between('#Ab6$', '3gJ^&')).toBe('+');
-  expect(lexirank.between('!', '!!O')).toBe('!!8');
-  expect(lexirank.between('}}', '}}O')).toBe('}}8');
-});
+test("between()", () => {
+	expect(lexirank.between("A", "C")).toBe("B")
+	expect(lexirank.between("#", "3")).toBe("+")
+	expect(lexirank.between("#Ab6$", "3gJ^&")).toBe("+")
+	expect(lexirank.between("!", "!!O")).toBe("!!8")
+	expect(lexirank.between("}}", "}}O")).toBe("}}8")
+})
 
-test('between() when there is no space', () => {
-  expect(lexirank.between('A', 'B')).toBe('AO');
-  expect(lexirank.between('AAA', 'AAB')).toBe('AAAO');
-  expect(lexirank.between('4', '5')).toBe('4O');
-});
+test("between() when there is no space", () => {
+	expect(lexirank.between("A", "B")).toBe("AO")
+	expect(lexirank.between("AAA", "AAB")).toBe("AAAO")
+	expect(lexirank.between("4", "5")).toBe("4O")
+})
 
-test('before()', () => {
-  expect(lexirank.before('O')).toBe('8');
-  expect(lexirank.before('#')).toBe('"');
-  expect(lexirank.before('"')).toBe('!O');
-  expect(lexirank.before('!"')).toBe('!!O');
-  expect(lexirank.before('!!"')).toBe('!!!O');
-});
+test("before()", () => {
+	expect(lexirank.before("O")).toBe("8")
+	expect(lexirank.before("#")).toBe('"')
+	expect(lexirank.before('"')).toBe("!O")
+	expect(lexirank.before('!"')).toBe("!!O")
+	expect(lexirank.before('!!"')).toBe("!!!O")
+})
 
-test('after()', () => {
-  expect(lexirank.after('O')).toBe('f');
-  expect(lexirank.after('|')).toBe('}');
-  expect(lexirank.after('}')).toBe('}O');
-  expect(lexirank.after('}}')).toBe('}}O');
-  expect(lexirank.after('}}}')).toBe('}}}O');
-});
+test("after()", () => {
+	expect(lexirank.after("O")).toBe("f")
+	expect(lexirank.after("|")).toBe("}")
+	expect(lexirank.after("}")).toBe("}O")
+	expect(lexirank.after("}}")).toBe("}}O")
+	expect(lexirank.after("}}}")).toBe("}}}O")
+})
 
-test('before() works the same as between() with only next', () => {
-  expect(lexirank.before('A')).toBe(lexirank.between(undefined, 'A'));
-});
+test("before() works the same as between() with only next", () => {
+	expect(lexirank.before("A")).toBe(lexirank.between(undefined, "A"))
+})
 
-test('after() works the same as between() with only prev', () => {
-  expect(lexirank.after('A')).toBe(lexirank.between('A', undefined));
-});
+test("after() works the same as between() with only prev", () => {
+	expect(lexirank.after("A")).toBe(lexirank.between("A", undefined))
+})
 
-test('mid() works the same as between() without arguments', () => {
-  expect(lexirank.mid()).toBe(lexirank.between());
-});
+test("mid() works the same as between() without arguments", () => {
+	expect(lexirank.mid()).toBe(lexirank.between())
+})
 
-test('throws if non-string passed', () => {
-  expect(() => lexirank.between('A', 3)).toThrow(
-    `prev and next can only be string`,
-  );
-  expect(() => lexirank.between(3, 'A')).toThrow(
-    `prev and next can only be string`,
-  );
-});
+test("throws if non-string passed", () => {
+	expect(() => lexirank.between("A", 3)).toThrow(
+		`prev and next can only be string`,
+	)
+	expect(() => lexirank.between(3, "A")).toThrow(
+		`prev and next can only be string`,
+	)
+})
 
-test('throws if empty string passed', () => {
-  expect(() => lexirank.between('A', '')).toThrow(
-    `prev and next can't be an empty string`,
-  );
-  expect(() => lexirank.between('', 'A')).toThrow(
-    `prev and next can't be an empty string`,
-  );
-});
+test("throws if empty string passed", () => {
+	expect(() => lexirank.between("A", "")).toThrow(
+		`prev and next can't be an empty string`,
+	)
+	expect(() => lexirank.between("", "A")).toThrow(
+		`prev and next can't be an empty string`,
+	)
+})
 
-test('throws if arguments are the same', () => {
-  expect(() => lexirank.between('A', 'A')).toThrow(
-    `prev should be smaller than next`,
-  );
-  expect(() => lexirank.between('AAAAAAA', 'AAAAAAA')).toThrow(
-    `prev should be smaller than next`,
-  );
-});
+test("throws if arguments are the same", () => {
+	expect(() => lexirank.between("A", "A")).toThrow(
+		`prev should be smaller than next`,
+	)
+	expect(() => lexirank.between("AAAAAAA", "AAAAAAA")).toThrow(
+		`prev should be smaller than next`,
+	)
+})
 
-test('throws if prev > next', () => {
-  expect(() => lexirank.between('B', 'A')).toThrow(
-    `prev should be smaller than next`,
-  );
-});
+test("throws if prev > next", () => {
+	expect(() => lexirank.between("B", "A")).toThrow(
+		`prev should be smaller than next`,
+	)
+})
 
-test('throws if invalid character passed', () => {
-  expect(() => lexirank.between('A', 'ß')).toThrow(
-    `contains invalid characters`,
-  );
-});
+test("throws if invalid character passed", () => {
+	expect(() => lexirank.between("A", "ß")).toThrow(
+		`contains invalid characters`,
+	)
+})
